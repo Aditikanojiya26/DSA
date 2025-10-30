ebddb7f461a306fbf2a0a7b212bf6f8ad391e828
Aditikanojiya26 <alpesh.food@gmail.com>
2025-10-30 13:28:27 +0530

    Time: 3 ms (82.27%), Space: 44.5 MB (97.25%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,31 +1,29 @@
 class Solution {
-    public static int value(char c){
-        switch(c){
-                case 'I': 
-                    return 1;
-                case 'V' :
-                    return 5;
-                case 'X' :
-                    return 10;
-                case 'L' :
-                    return 50;
-                case 'C' :
-                    return 100;
-                case 'D' :
-                    return 500;
-                case 'M' :
-                    return 1000;
-            }
-            return 0;
+    private static int value(char c) {
+        switch (c) {
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+            default: return 0;
+        }
     }
+
     public int romanToInt(String s) {
-        int prev=value(s.charAt(0));
-        int sum=prev;
-        for(int i=1;i<s.length();i++){
-            int current=value(s.charAt(i));
-            if(prev<current)sum=(sum-prev)+(current-prev);
-            else sum+=current;
-            prev=current;
+        int sum = 0;
+        int n = s.length();
+
+        for (int i = 0; i < n; i++) {
+            int curr = value(s.charAt(i));
+            // If the next value is larger, subtract current value
+            if (i + 1 < n && curr < value(s.charAt(i + 1))) {
+                sum -= curr;
+            } else {
+                sum += curr;
+            }
         }
         return sum;
     }
