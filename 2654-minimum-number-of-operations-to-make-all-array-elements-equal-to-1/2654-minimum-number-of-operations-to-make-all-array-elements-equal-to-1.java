2c6089df21bb00c9e4bce4640e0dcde98f98db71
Aditikanojiya26 <alpesh.food@gmail.com>
2025-11-12 13:36:24 +0530

    Time: 1 ms (100%), Space: 44.1 MB (8.22%) - LeetHub

---
+++ 2654-minimum-number-of-operations-to-make-all-array-elements-equal-to-1/2654-minimum-number-of-operations-to-make-all-array-elements-equal-to-1.java
@@ -0,0 +1,31 @@
+class Solution {
+    public static int gcd(int a, int b) {
+        while(b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+    public int minOperations(int[] nums) {
+        int n= nums.length;
+        int count = 0;
+        for( int i = 0 ; i < n ; i++){
+            if( nums[i] == 1) count++;
+        }
+        if( count > 0 ) return n - count;
+        int smSbLn=Integer.MAX_VALUE;
+        for(int i =0 ; i < n-1 ; i++){
+            int value=nums[i];
+            for ( int j = i ; j < n ; j++){
+                value=gcd(value,nums[j]);
+                if (value == 1 ) {
+                    smSbLn = Math.min(smSbLn, (j - i + 1)); 
+                    break; 
+                }
+            }
+        }
+        if(smSbLn!=Integer.MAX_VALUE) return smSbLn+n-2;
+        return -1;
+    }
+}
