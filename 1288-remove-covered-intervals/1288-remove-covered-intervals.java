a190bcfd242c935683bade66e2b645e8311b276b
Aditikanojiya26 <alpesh.food@gmail.com>
2025-12-31 09:49:09 +0530

    Time: 7 ms (46.41%), Space: 46.2 MB (95.49%) - LeetHub

---
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int removeCoveredIntervals(int[][] intervals) {
+        Arrays.sort(intervals,(a,b)->a[0]-b[0]);
+        int count = 1;
+        int open = intervals[0][0];
+        int close = intervals[0][1];
+        
+        for ( int i = 1 ; i < intervals.length ; i++){
+           int curopen = intervals[i][0];
+           int curclose = intervals[i][1];
+           int c = intervals[i][1];
+           if(curopen <= open && curclose >= close){
+                open = intervals[i][0];
+                close = intervals[i][1];
+           }
+           else if( !(open <= curopen &&  curclose <= close)){
+            count++;
+            open = intervals[i][0];
+            close = intervals[i][1];
+           }
+           
+        }
+        return count;
+    }
+}
