5abee9c8e22177f975cf39276eae73d5d67846a1
Aditikanojiya26 <alpesh.food@gmail.com>
2026-01-15 10:08:28 +0530

    Time: 11 ms (94.81%), Space: 47.7 MB (38.29%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,25 +1,31 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        Map<Integer, Integer> map = new HashMap<>();
-        
-        // 1. Count frequencies
-        for (int n : nums) { 
-            map.put(n, map.getOrDefault(n, 0) + 1);
+        HashMap<Integer , Integer> map = new HashMap<>();
+        List<Integer>[] bucket = new List[nums.length+1];
+
+        for(int i : nums){
+            map.put(i , map.getOrDefault(i,0)+1);
         }
 
-        // 2. Convert entries to a list for sorting
-        List<Map.Entry<Integer, Integer>> list = new ArrayList<>(map.entrySet());
-        
-        // 3. Sort by value descending (High frequency to Low)
-        list.sort((a, b) -> b.getValue().compareTo(a.getValue()));
-
-        // 4. Extract the first K keys
-        int[] arr = new int[k];
-        for (int i = 0; i < k; i++) {
-            // Get the key from the i-th entry in the sorted list
-            arr[i] = list.get(i).getKey();
+        for(int key : map.keySet()){
+            int frequency = map.get(key);
+            if(bucket[frequency] == null){
+                bucket[frequency] = new ArrayList<>();
+            }
+            bucket[frequency].add(key);
         }
-        
-        return arr;
+
+        int[] res = new int[k];
+        int counter = 0;
+
+        for(int i = bucket.length-1;i>=0 && counter<k;i--){
+            if(bucket[i] != null){
+                for(Integer ans : bucket[i]){
+                    res[counter++] = ans;
+                }
+            }
+        }
+
+        return res;
     }
 }
