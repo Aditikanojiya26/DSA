fbcdef1e1bc485442ae9b9a6841120b8505235ed
Aditikanojiya26 <alpesh.food@gmail.com>
2026-01-18 11:05:39 +0530

    Time: 1 ms (90.63%), Space: 42.7 MB (36.44%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.java
+++ 0029-divide-two-integers/0029-divide-two-integers.java
@@ -1,36 +1,27 @@
 class Solution {
     public int divide(int dividend, int divisor) {
-
-        if (dividend == Integer.MIN_VALUE && divisor == -1)
+        if (dividend == Integer.MIN_VALUE && divisor == -1) {
             return Integer.MAX_VALUE;
-
-       
-        if (dividend == divisor) return 1;
-
+        }
+        if (dividend == Integer.MIN_VALUE && divisor == 1) {
+            return Integer.MIN_VALUE;
+        }
+        int count = 0;
+        boolean flag = true;
         
-        boolean sign = (dividend >= 0 && divisor >= 0) || (dividend < 0 && divisor < 0);
-
-  
+        if( divisor < 0 &&  dividend < 0) flag = true;
+        else if (divisor < 0 ||  dividend < 0)flag=false;
+        // if( divisor < 0) divisor = -(divisor);
+        // if( dividend < 0) dividend = -(dividend);
         long a = Math.abs((long) dividend);
         long b = Math.abs((long) divisor);
 
-        int ans = 0;
-        
-        while (a >= b) {
-            long temp = b;
-            long multiple = 1;
-
-            
-            while (a >= (temp << 1)) {
-                temp <<= 1;
-                multiple <<= 1;
-            }
-
-            a -= temp;
-            ans += multiple;
+        while(a >= b){
+            long pow = 1;
+            while( a >= b * (pow*2)) pow*=2;
+            count+=pow;
+            a -= b*pow;
         }
-
-       
-        return sign ? ans : -ans;
+        return flag ? count : -count ;
     }
 }
