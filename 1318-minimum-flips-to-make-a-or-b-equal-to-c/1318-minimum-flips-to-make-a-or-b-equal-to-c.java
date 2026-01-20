7dd2dd7318de07b6ddd81690f8e75f8392b3da7a
Aditikanojiya26 <alpesh.food@gmail.com>
2026-01-20 08:43:39 +0530

    Time: 0 ms (100%), Space: 42.3 MB (14.68%) - LeetHub

--- 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.java
+++ 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.java
@@ -1,52 +1,24 @@
 class Solution {
-
-    public static String decimalToBinary(int n) {
-        if (n == 0) return "0";
-
-        StringBuilder binary = new StringBuilder();
-        while (n > 0) {
-            binary.append(n % 2);
-            n /= 2;
-        }
-        return binary.reverse().toString();
-    }
-
-    private String padLeft(String s, int len) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = s.length(); i < len; i++) {
-            sb.append('0');
-        }
-        sb.append(s);
-        return sb.toString();
-    }
-
     public int minFlips(int a, int b, int c) {
-        String one = decimalToBinary(a);
-        String two = decimalToBinary(b);
-        String three = decimalToBinary(c);
-        String res = decimalToBinary(a | b);
-
-        int maxLen = Math.max(
-            Math.max(one.length(), two.length()),
-            Math.max(three.length(), res.length())
-        );
-
-        one = padLeft(one, maxLen);
-        two = padLeft(two, maxLen);
-        three = padLeft(three, maxLen);
-        res = padLeft(res, maxLen);
-
         int count = 0;
 
-        for (int i = 0; i < maxLen; i++) {
-            if (three.charAt(i) != res.charAt(i)) {
-                if (three.charAt(i) == '0') {
-                    if (one.charAt(i) == '1') count++;
-                    if (two.charAt(i) == '1') count++;
-                } else {
-                    count++;
-                }
+        while (a > 0 || b > 0 || c > 0) {
+            int bitA = a & 1;
+            int bitB = b & 1;
+            int bitC = c & 1;
+
+            if (bitC == 0) {
+                // both must be 0
+                if (bitA == 1) count++;
+                if (bitB == 1) count++;
+            } else {
+                // at least one must be 1
+                if (bitA == 0 && bitB == 0) count++;
             }
+
+            a >>= 1;
+            b >>= 1;
+            c >>= 1;
         }
 
         return count;
