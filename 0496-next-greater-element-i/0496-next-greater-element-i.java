4c2f11cfed3bb24543ee61e5c3c62c6b2cdb40cc
Aditikanojiya26 <alpesh.food@gmail.com>
2026-01-23 10:17:31 +0530

    Time: 4 ms (73.56%), Space: 45.3 MB (41.47%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,25 +1,21 @@
 class Solution {
-    
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        int n2 = nums2.length;
-        int[] result = new int[n2];
-        Arrays.fill(result, -1);
         Stack<Integer> stack = new Stack<>();
-        HashMap<Integer,Integer> map = new HashMap<>();
-        for( int i = 0 ; i < n2 ; i++){
-            while( !stack.isEmpty() && nums2[i]>nums2[stack.peek()]){
-                int index = stack.pop();
-                result[index] = nums2[i];
+        HashMap<Integer, Integer> map = new HashMap<>(); // number -> next greater
+
+        for (int num : nums2) {
+            while (!stack.isEmpty() && num > stack.peek()) {
+                map.put(stack.pop(), num);
             }
-            stack.push(i);
-            map.put(nums2[i],i);
+            stack.push(num);
         }
-        int n1 = nums1.length;
-        int[] fresult = new int[n1];
-        for( int i = 0 ; i < n1 ; i++){
-            int index = map.get(nums1[i]);
-            fresult[i] = result[index];
+
+        // remaining elements in stack have no next greater, default -1
+        int[] fresult = new int[nums1.length];
+        for (int i = 0; i < nums1.length; i++) {
+            fresult[i] = map.getOrDefault(nums1[i], -1);
         }
+
         return fresult;
     }
 }
