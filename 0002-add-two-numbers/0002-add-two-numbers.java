149dc179defec08b0b8dc1e54dae7c1036695331
Aditikanojiya26 <alpesh.food@gmail.com>
2026-03-04 11:17:51 +0530

    Time: 1 ms (99.82%), Space: 46.6 MB (57.37%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -1,55 +1,30 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        int sum = l1.val + l2.val;
-        boolean flag = sum >= 10 ? true : false ;
-        ListNode head = new ListNode(sum%10);
-        ListNode temp = head;
-        ListNode temp1 = l1.next;
-        ListNode temp2 = l2.next;
-        while(temp1!=null && temp2!=null){
-            sum = temp1.val + temp2.val;
-            int ans = ((sum) + (flag ? 1 : 0))%10;
-            temp.next = new ListNode(ans);
-            temp = temp.next;
-            flag = (sum) + (flag ? 1 : 0) >= 10 ? true : false;  
-            temp1 = temp1.next;
-            temp2 = temp2.next;
-        }
-        if(temp1!=null){
-        while(temp1!=null){
-            sum = temp1.val;
-            int ans =( (sum) + (flag ? 1 : 0))%10;
-            temp.next = new ListNode(ans);
-            temp = temp.next;
-            flag = (sum) + (flag ? 1 : 0) >= 10 ? true : false;  
-            temp1 = temp1.next;
-        }
-        }
-        else{
-        while(temp2!=null){
-            sum = temp2.val;
-            int ans = ( (sum) + (flag ? 1 : 0))%10;
-            temp.next = new ListNode(ans);
-            temp = temp.next;
-            flag =(sum) + (flag ? 1 : 0) >= 10 ? true : false; 
-            temp2 = temp2.next;
-        }
-        
 
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+
+        int carry = 0;
+
+        while (l1 != null || l2 != null || carry != 0) {
+
+            int sum = carry;
+
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+
+            carry = sum / 10;
+            current.next = new ListNode(sum % 10);
+            current = current.next;
         }
-        if(flag==true){
-             temp.next = new ListNode(1);
-        }
-        return head;
+
+        return dummy.next;
     }
 }
